fa9b1132658b888aa1668894cb97199cd1284cea
Faethon Milikouris <faethon.milikouris@gmail.com>
2016-02-24 19:12:00 -0800

    add update action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,15 +6,24 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    user = User.new (user_params)
+    user = User.new(user_params)
     if user.save
       render json: user, status: 201, location: [:api, user]
     else
-      render json: {errors: user.errors}, status: 422
+      render json: { errors: user.errors }, status: 422
     end
   end
 
-  private 
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: { status: 200 }, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:email,
