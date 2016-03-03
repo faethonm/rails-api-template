77baa234dece41afe2efba6aac3d86af0836dc80
Faethon Milikouris <faethon.milikouris@gmail.com>
2016-03-02 19:01:39 -0800

    add authentication token to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
   before { @user = FactoryGirl.build(:user) }
 
   subject { @user }
-
+  let(:token) { 'token12345'}
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
@@ -12,22 +12,22 @@ describe User do
   it { should be_valid }
 
   it { should validate_presence_of(:email) }
-  
+
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
 
   it { should respond_to(:auth_token) }
   it { should validate_uniqueness_of(:auth_token) }
 
-  describe "#generate authentication token for user!" do
+  describe '#generate authentication token for user!' do
     it 'generates an auth token' do
-      Devise.stub(:friendly_token).and_return('token12345')
+      Devise.stub(:friendly_token).and_return(token)
       @user.generate_authentication_token!
-      expect(@user.auth_token).to eq('token12345')
+      expect(@user.auth_token).to eq(token)
     end
 
-    it "generates another token when one already has been taken" do
-      existing_user = FactoryGirl.create(:user, auth_token: "token12345")
+    it 'generates another token when one already has been taken' do
+      existing_user = FactoryGirl.create(:user, auth_token: token)
       @user.generate_authentication_token!
       expect(@user.auth_token).not_to eql existing_user.auth_token
     end
