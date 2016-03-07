3a7997c2434f30cedc62f80adcb2b9eaf70ae78f
Faethon Milikouris <faethon.milikouris@gmail.com>
2016-03-06 16:07:28 -0800

    Add authenticable module to handle authentication between calls

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,24 +1,36 @@
-class Api::V1::SessionsController < ApplicationController
-  def create
-    password = params[:session][:password]
-    email = params[:session][:email]
-    user = email.present? && User.find_by(email: email)
+module Api
+  module V1
+    # Controller to handle sign in and sign out actions
+    class SessionsController < ApplicationController
 
-    if user.valid_password? password
-      sign_in user, store: false
-      user.generate_authentication_token!
-      user.save!
-      render json: user, status: 200, location: [:api, user]
-    else
-      render json: {errors: 'Invalid email or password'}, status: 422
+      #Sign in user
+      def create
+        password = params[:session][:password]
+        email = params[:session][:email]
+        user = email.present? && User.find_by(email: email)
+
+        if user.valid_password? password
+          sign_in_user(user)
+          render json: user, status: 200, location: [:api, user]
+        else
+          render json: { errors: 'Invalid email or password' }, status: 422
+        end
+      end
+
+      #Sign out user
+      def destroy
+        user = User.find_by(auth_token: params[:id])
+        user.generate_authentication_token!
+        user.save
+      end
+
+      private
+
+      def sign_in_user(user)
+        sign_in user, store: false
+        user.generate_authentication_token!
+        user.save!
+      end
     end
   end
-
-  def destroy
-    user = User.find_by(auth_token: params[:id])
-    user.generate_authentication_token!
-    user.save
-    head 204
-  end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,12 +27,13 @@ end
 
 RSpec.configure do |config|
   #Including to test requests
+
   config.include Request::JsonHelpers, :type => :controller
   config.include Request::HeadersHelpers, :type => :controller
   config.before(:each, type: :controller) do
     include_default_accept_headers
   end
-  
+
   #Devise helpers
   config.include Devise::TestHelpers, :type => :controller
   # ## Mock Framework
